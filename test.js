c6b7eba27da0d37cb0834d72a44a36de73e191a1
martindeeangelo <deepmartin0@gmail.com>
2026-01-26 21:32:38 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 console.log('✅ player boot', location.pathname);
+(() => {
+  const BOOT = () => {
+    console.log('✅ player boot', location.pathname);
+
+    // tu potem wejdzie: wykrywanie kafelków/listy + MP3 per produkt + sticky
+  };
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', BOOT);
+  } else {
+    BOOT();
+  }
+})();
